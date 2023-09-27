f60f0d9f324c39c537255fc4ba510a4aab8b4fd7
sanghyeon <uphyeon@kakao.com>
2023-09-27 17:01:33 +0900

    공통 레이아웃 댓글 기능 추가

--- js/details.js
+++ js/details.js
@@ -21,20 +21,80 @@ const MEMBER_MAP = {
   },
 };
 
-$(document).ready(function () {
-  const fileName = location.pathname.split('/').pop().replace(/.html/g, '');
+// Firebase SDK 라이브러리 가져오기
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
+import {getFirestore} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import {collection, addDoc} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import {getDocs} from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
-  for (let member in MEMBER_MAP) {
-    let {name, color, img} = MEMBER_MAP[member];
-    if (member === fileName) continue;
-    $('#memberButtonBox').append(
-      `<div class="name-box">
+// Firebase 구성 정보 설정
+const firebaseConfig = {
+  apiKey: 'AIzaSyBuU6pe2VYUIoalynNO4w25HcfuZOI0Ig8',
+  authDomain: 'sparta-bada5.firebaseapp.com',
+  projectId: 'sparta-bada5',
+  storageBucket: 'sparta-bada5.appspot.com',
+  messagingSenderId: '678152779933',
+  appId: '1:678152779933:web:fd4d686066c4bbbf7dbc74',
+  measurementId: 'G-J9PY997T6X',
+};
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 화면에 표시될 멤버 이름
+let memberName = '';
+
+// 댓글 입력 이벤트
+$('#commentEnterButton').click(async function () {
+  let comment = $('#commentInput').val();
+  let date = new Date().toLocaleString();
+
+  let doc = {
+    comment,
+    memberName,
+    date,
+  };
+
+  await addDoc(collection(db, 'comments'), doc);
+  $('#commentInput').val('');
+  alert('댓글이 등록 되었습니다.');
+  readComments(memberName);
+});
+
+const readComments = async () => {
+  // 전체 댓글 불러오기
+  let docs = await getDocs(collection(db, 'comments'));
+
+  docs.forEach(doc => {
+    let row = doc.data();
+    if (row) {
+      // 화면의 사용자에 대한 댓글만 보여
+      if (row.memberName === memberName)
+        $('#commentList').append(`<div><span>${row.comment}</span><span>${row.date}</span></div>`);
+    }
+  });
+};
+
+// 표시되고 있는 html 파일 이름
+const fileName = location.pathname.split('/').pop().replace(/.html/g, '');
+
+// 우측 상단 멤버 버튼 만들기
+for (let member in MEMBER_MAP) {
+  let {name, color, img} = MEMBER_MAP[member];
+  if (member === fileName) {
+    $('#memberName').text(name);
+    memberName = name;
+    readComments(memberName);
+    continue;
+  }
+  $('#memberButtonBox').append(
+    `<div class="name-box">
         <a href=${
           member + '.html'
         }><img src="../assets/img/${img}" class="button member-image" style="background-color: ${color}" alt="${name}" />
         <div class="name" style="background-color: ${color}">${name}</div>
         </a>
       </div>`,
-    );
-  }
-});
+  );
+}
